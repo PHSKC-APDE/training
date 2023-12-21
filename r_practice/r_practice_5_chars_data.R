bea92e91234b77d027938540718015afa8750ee2
Ronald W. Buie <rbuie@kingcounty.gov>
2023-12-20 16:04:02 -0800

    updated preamble

--- r_practice/r_practice_5_chars_data.R
+++ r_practice/r_practice_5_chars_data.R
@@ -1,6 +1,23 @@
 ##RADS practice
 ##Analyzing CHARS data##
-##Eli Kern, June 2023
+##Ronald Buie, Dec 2023
+
+
+#### Background ####
+# The following is an R script introducing users to a basic analysis of APDE CHARS data. It walks through how to access CHARS data and perform some of our routine analyses.
+#
+# It is part of a larger set of training resources that can be found here: https://github.com/PHSKC-APDE/R_training
+#
+# This vignette relies on our in-house analytics package "R Analytics "R Automatic Data System" or RADS. You can find RADS and installation instructions here: <link to RADS repository
+#
+# This vignette assumes you have access to the necessary data. If you do not, or are not sure, please reach out to your manager.
+#
+# A recorded presentation of this script, with audience Q/A can be found here:
+#
+# You can find this, and the rest of our training scripts at https://github.com/PHSKC-APDE/R_training/tree/main/r_practice
+#
+# Based on vignette found at https://github.com/PHSKC-APDE/rads/wiki/chars_functions
+
 
 #### Setup ####
 
@@ -9,11 +26,10 @@
 remotes::update_packages("rads") #update RADS if it is out of date
 
 ##Load packages and set defaults
-pacman::p_load(tidyverse, rads, rads.data, openxlsx) # Load list of packages
+pacman::p_load(data.table, rads, rads.data, openxlsx) # Load list of packages
 options(max.print = 350) # Limit # of rows to show when printing/showing a data.frame
 options(tibble.print_max = 50) # Limit # of rows to show when printing/showing a tibble (a tidyverse-flavored data.frame)
 options(scipen = 999) # Avoid scientific notation
 origin <- "1970-01-01" # Set the origin date, which is needed for many data/time functions
-export_path <- "C:/Users/REPLACE WITH YOUR USER NAME/OneDrive - King County/" #replace with your desired path, use forward slashes
 
-##Copy code from existing vignette at: https://github.com/PHSKC-APDE/rads/wiki/chars_functions
+
