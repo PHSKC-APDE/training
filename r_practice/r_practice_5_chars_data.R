522e01807cc22b6b9de72fec4311fed831b5fae5
Ronald W. Buie <rbuie@kingcounty.gov>
2023-12-26 17:32:08 -0800

    completed chars and circular population aggregations trainings

--- r_practice/r_practice_5_chars_data.R
+++ r_practice/r_practice_5_chars_data.R
@@ -17,6 +17,8 @@
 # You can find this, and the rest of our training scripts at https://github.com/PHSKC-APDE/R_training/tree/main/r_practice
 #
 # Based on vignette found at https://github.com/PHSKC-APDE/rads/wiki/chars_functions
+#
+# Background information upon which APDE's CHARS data classifications are based https://hcup-us.ahrq.gov/toolssoftware/ccsr/dxccsr.jsp
 
 
 #### Setup ####
@@ -32,4 +34,117 @@ options(tibble.print_max = 50) # Limit # of rows to show when printing/showing a
 options(scipen = 999) # Avoid scientific notation
 origin <- "1970-01-01" # Set the origin date, which is needed for many data/time functions
 
+
+###### Vignette ######
+
+#CHARS functions allow you to retrieve and analyze CHARS observations by ICD and CCSR classifications.
+
+#let's start by pulling data and looking at what is returned.
+
+charsDT <- get_data_chars(year = 2021) #pull data from chars
+
+dim(charsDT) # dimensions of the downloaded CHARS data.table object
+
+names(charsDT)[1:6] # names of the first 6 columns
+
+unique(charsDT$chi_geo_kc) # display unique values of the king count indicator to confirm data is limited to King County
+
+unique(charsDT$chi_year) # display unique values of the year variable (which years do we have)
+
+max(charsDT$chi_age, na.rm = T) # check that ages max to 100 (ADPE standard), na.rm will remove NA's from the calculation of the maximum. If you do not, you will receive NA, instead of anumber, if any.
+
+#### viewing possible codes and descriptors ####
+
+classifications <- chars_icd_ccs() #returns list of all descriptions
+
+#note that each has the relevant icd code, a broad CCSR description, detailed CCSR description, and icd version. You can use any of these for aggregating with the below functions.
+classifications[1:5]
+
+classifications[icdcm_code == "I110",] #see information for a code of interest
+
+
+#### getting CHARS counts ####
+
+mycode <- chars_icd_ccs_count(ph.data = charsDT, icdcm = 'I110') #using our data, charsDT, we can specify a valid icd code to display how many observations meet the code
+
+mycode
+
+mydesc <- chars_icd_ccs_count(ph.data = charsDT, icdcm = 'hypertensive heart disease with heart failure') #you can also use the description
+
+mydesc
+
+broad <- chars_icd_ccs_count(ph.data = charsDT, broad = 'Diseases of the circulatory system') # and, similarly, by APDE's CCSR aligned broad descriptions
+
+broad
+
+### listing injury options with  chars_injury_matrix() ###
+
+
+injuries <- chars_injury_matrix()
+
+injuries[1:10]
+
+unique(injuries$intent) # see a list of the available intents
+
+unique(injuries$mechanism) #Similarly, to see the available mechanisms
+
+### generate counts of injuries from our data
+
+mat1 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'assault',
+                                  mechanism = 'none')
+
+mat1
+
+mat2 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'assault|undetermined', #note you can use logic operators, such as OR used here
+                                  mechanism = 'none')
+
+mat2
+
+
+mat2.alt <- chars_injury_matrix_count(ph.data = charsDT,
+                                      intent = c('assault', 'undetermined'), #will pull same results
+                                      mechanism = 'none')
+
+identical(mat2, mat2.alt)
+
+## Specifying a single mechanism and ignoring the intent
+
+mat3 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'none',
+                                  mechanism = 'motor_vehicle_traffic')
+
+mat3
+
+## What happens if you specify ‘none’ for both the mechanism and intent?
+
+mat4 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'none',
+                                  mechanism = 'none')
+
+## What happens if you don’t specify the mechanism and intent?
+
+mat5 <- chars_injury_matrix_count(ph.data = charsDT)[1:10]
+
+## How different are the `narrow` and `broad` definitions?
+
+mat6 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'none',
+                                  mechanism = 'none',
+                                  def = 'narrow')
+
+mat7 <- chars_injury_matrix_count(ph.data = charsDT,
+                                  intent = 'none',
+                                  mechanism = 'none',
+                                  def = 'broad')
+
+mat6
+
+mat7
+
+deftable <- rbind(cbind(def = 'narrow', mat6),
+                  cbind(def = 'broad', mat7))
+
+deftable v#notice this is quite different. Happy to discuss, but basically you should be familiar with which AHRQ/CCSR and/or ICD classifications to use for your analysis, and be intentional about using the functions to aggregate correctly.
 
--- r_practice/r_practice_8_make_a_map_using_birth_data.R
+++ r_practice/r_practice_8_make_a_map_using_birth_data.R
@@ -20,13 +20,13 @@ export_path <- "C:/Users/REPLACE WITH YOUR USER NAME/OneDrive - King County/" #r
 ##Goal: Make an HRA-based map of smoking during pregnancy for the most recent 10-year period
 
 ##Load birth data for 2012-2021
-birth <- get_data_birth(cols = c("chi_year", "smoking_dur", "chi_geo_hra2020_id", "chi_geo_hra2020_name"), 
-                        year = c(2012:2021), 
+birth <- get_data_birth(cols = c("chi_year", "smoking_dur", "chi_geo_hra2020_id", "chi_geo_hra2020_name"),
+                        year = c(2012:2021),
                         kingco = T)
 
 ##Use calc to generate proportion of births where mother smoked during pregnancy, by HRA
-results <- calc(ph.data = birth, 
-     what = c("smoking_dur"), 
+results <- calc(ph.data = birth,
+     what = c("smoking_dur"),
      metrics = c("mean", "rse", "numerator", "denominator"),
      by = c("chi_geo_hra2020_id", "chi_geo_hra2020_name"),
      time_var = "chi_year")
@@ -34,12 +34,12 @@ results <- calc(ph.data = birth,
 ##Convert proportions to percentages, and suppress small numbers per APDE guidelines (i.e., 1-9)
 results <- results %>%
   mutate(
-    
+
     #Suppress mean for any rows where numerator between 1 and 9
     mean = case_when(
       between(numerator, 1, 9) ~ NA_real_,
       TRUE ~ mean),
-    
+
     #Make percentage variables (note you can also do this within chart creation script)
     mean_percent = rads::round2(mean*100,1),
     mean_lower_percent = rads::round2(mean_lower*100,1),
@@ -76,22 +76,22 @@ centroids <- cbind(centroids, st_coordinates(st_centroid(centroids$geometry)))
 
 ##Set up map for plotting
 #The order of data layers is important because they are stacked on top of each other
-my.map1 <- ggplot() + 
+my.map1 <- ggplot() +
   geom_sf(data = hra2020, fill = 'White', color = NA) + #White color for water and other blank areas, no outline
   geom_sf(data = map, aes(fill=mean_percent)) + #Color HRAs according to mean_percent
   geom_sf(data = hra2020, fill = NA, size = 1, color = 'black') + #Black outlines for HRAs
   geom_label(data = centroids, aes(x=X, y=Y, label = as.character(NAME10)), size = 5, color = 'Black',
              label.padding = unit(0.1, "lines")) + #Labels for cities of interest
   geom_label_repel()+ #Ask labels to avoid overlapping
-  
+
   labs(title = "Percentage of births where mother smoked during pregnancy, by King County HRA, 2012-2021 average") +
-  
+
   theme(plot.title = element_text(color = "black", size = 16, face = "bold"), #Format title
         axis.line = element_blank(), axis.text = element_blank(), #Suppress axis line
         axis.ticks = element_blank(), axis.title = element_blank(), #Suppress axis ticks and labels
         panel.grid.major = element_line(color = "white"), #Format background outline
         panel.background = element_rect(fill = "white")) + #Format backgound fill
-  
+
   #Specify a manual color palette (this is APDE standard for CHI sequential color scale in maps)
   scale_fill_gradientn(
     colors = c("#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1","#88419d", "#810f7c", "#4d004b"), #List colors
@@ -99,7 +99,7 @@ my.map1 <- ggplot() +
     na.value = "grey50", #Color NA values
     guide = 'colourbar', #Type of legend
     name = "Percentage") #Title of legend
-  
+
   #Another example of a color palette from built-in Tableau options
   #scale_fill_gradient2_tableau(palette = "Classic Orange-Blue",  na.value = 'White', guide = 'colourbar', trans='reverse',
   #                           name = "Percentage") #Color scale, specifying white for suppressed areas
